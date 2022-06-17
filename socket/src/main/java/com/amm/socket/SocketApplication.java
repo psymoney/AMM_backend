9adf097eff18bd4437d48435bb8816783af9605d
psymoney <sangykimiv@gmail.com>
2022-06-17 16:57:25 +0900

    [feat] add scheduler configuration annotation

--- socket/src/main/java/com/amm/socket/SocketApplication.java
+++ socket/src/main/java/com/amm/socket/SocketApplication.java
@@ -1,8 +1,13 @@
 package com.amm.socket;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableScheduling
 @SpringBootApplication
 public class SocketApplication {
 
