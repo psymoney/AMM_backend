047b33ac7abfc5ba25f82e7a81e886188c318ffd
psymonkey <sangykimiv@gmail.com>
2022-06-19 02:23:39 +0900

    [feat] initiate mapper and vo for DayCandlePrice

---
+++ socket/src/main/java/com/amm/socket/mapper/DayCandlePrice.java
@@ -0,0 +1,10 @@
+package com.amm.socket.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface DayCandlePrice {
+
+}
