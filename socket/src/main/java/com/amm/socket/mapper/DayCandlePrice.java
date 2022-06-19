3a10f198ff845d3a573d9df4d74d179b281d205c
psymonkey <sangykimiv@gmail.com>
2022-06-19 22:41:25 +0900

    [feat] add DayCandleMapper and upsertion query

--- socket/src/main/java/com/amm/socket/mapper/DayCandlePrice.java
+++ socket/src/main/java/com/amm/socket/mapper/DayCandlePrice.java
@@ -1,10 +1,11 @@
 package com.amm.socket.mapper;
 
+import com.amm.socket.vo.DayCandlePriceVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface DayCandlePrice {
-
+    void upsertCandlePrice(DayCandlePriceVO dayCandlePriceVO);
 }
