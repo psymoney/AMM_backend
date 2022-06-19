f521abd8b87ac88e1dc4fc20d59780b360410044
psymonkey <sangykimiv@gmail.com>
2022-06-20 03:11:59 +0900

    [feat] create initial controller

---
+++ rest/src/main/java/com/amm/rest/controller/DayCandlePriceController.java
@@ -0,0 +1,10 @@
+package com.amm.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("candle")
+public class DayCandlePriceController {
+    
+}
