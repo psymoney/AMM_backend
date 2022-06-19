c9f944ab88069408dbec998e5f56cb56285b1ef6
psymonkey <sangykimiv@gmail.com>
2022-06-20 03:47:11 +0900

    [feat] add controller

--- rest/src/main/java/com/amm/rest/controller/DayCandlePriceController.java
+++ rest/src/main/java/com/amm/rest/controller/DayCandlePriceController.java
@@ -1,10 +1,37 @@
 package com.amm.rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.amm.rest.service.DayCandlePriceService;
+import com.amm.rest.vo.DayCandlePriceVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("candle")
+@RequestMapping("/day/candle")
 public class DayCandlePriceController {
+    @Autowired
+    DayCandlePriceService dayCandlePriceService;
+
+    @PostMapping(value = "/day", produces = "application/json; charset=utf-8")
+    public ResponseEntity<DayCandlePriceVO> getCandlePrice(@RequestBody Map<String, String> param) {
+        DayCandlePriceVO result = dayCandlePriceService.getCandlePrice(param);
+        if (result == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/period", produces = "application/json; charset=utf-8")
+    public ResponseEntity<DayCandlePriceVO[]> getCandlePricesDuringPeriod(@RequestBody Map<String, String> params) {
+        DayCandlePriceVO[] result = dayCandlePriceService.getCandlePricesDuringPeriod(params);
+        if (result == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
     
 }
